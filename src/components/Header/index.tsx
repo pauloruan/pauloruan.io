f1ab97832bdd45029687ded164e9dbc571095e17
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 17:02:56 -0300

    refactor: `Header` style changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
-import { navLinks } from "@utils/navLinks"
 import { ButtonToggleTheme } from "@components/ButtonToggleTheme"
 import { Menu } from "@components/Menu"
+import { navLinks } from "@utils/navLinks"
 
 export function Header(): JSX.Element {
   return (
-    <header className="box-border w-full h-12 flex flex-row flex-nowrap items-center justify-between px-2 md:px-32">
+    <header className="box-border w-full md:w-[80vw] h-12 flex flex-row flex-nowrap items-center justify-between px-1 sm:px-8 md:px-24 lg:px-36">
       <Menu links={navLinks} />
-      <ButtonToggleTheme />
+      <div className="flex flex-row justify-between items-center text-center gap-2">
+        <ButtonToggleTheme />
+      </div>
     </header>
   )
 }
