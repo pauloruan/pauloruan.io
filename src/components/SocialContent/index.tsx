66f5e98b46d65b032c66ec0a7b9a470414f4bb76
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 17:10:01 -0300

    refactor: `SocialContent` component styling changes

--- src/components/SocialContent/index.tsx
+++ src/components/SocialContent/index.tsx
@@ -3,25 +3,25 @@ import { FaGithub, FaLinkedin } from "react-icons/fa"
 
 export function SocialContent(): JSX.Element {
   return (
-    <ul className="flex flex-row items-center justify-between w-auto h-full">
-      <li className="mr-1 md:mr-2 list-none">
+    <ul className="max-w-max h-full flex flex-row items-center justify-between gap-4 my-2">
+      <li className="list-none">
         <Link
           href="https://linkedin.com/in/pauloruan/"
           target="_blank"
           aria-label="LinkedIn Profile"
           className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
         >
-          <FaLinkedin className="text-lg mr-1 md:text-2xl md:mr-2" />
+          <FaLinkedin className="text-lg md:text-2xl" />
         </Link>
       </li>
-      <li className="ml-1 md:ml-2 list-none">
+      <li className="list-none">
         <Link
           href="https://github.com/pauloruan/"
           target="_blank"
           aria-label="GitHub Profile"
           className="text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
         >
-          <FaGithub className="text-lg ml-1 md:text-2xl md:ml-2" />
+          <FaGithub className="text-lg md:text-2xl" />
         </Link>
       </li>
     </ul>
