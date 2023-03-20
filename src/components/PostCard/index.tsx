0713115d9082bb9c07bec2b12050d6809543f37d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 18:58:17 -0300

    refactor: `PostCard` component styling changes

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -4,21 +4,16 @@ import Link from "next/link"
 
 export function PostCard(props: FormattedPost): JSX.Element {
   return (
-    <div className="max-w-sm md:max-w-3xl overflow-hidden flex flex-col items-start justify-between my-4 py-2">
-      <Link href={`/blog/${props.slug}`}>
-        <h5 className="min-w-max md:mr-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white hover:underline decoration-2 transition duration-300 ease-in-out">
-          {props.title}
-        </h5>
-      </Link>
-      <div className="flex flex-row md:items-center md:justify-between justify-start min-w-max my-2 md:text-sm text-xs">
-        <p className="font-semibold text-gray-500 dark:text-gray-400">
-          {readingTime(props.body)}
-        </p>
-        &nbsp; • &nbsp;
-        <p className="font-semibold text-gray-500 dark:text-gray-400">
-          {props.date}
-        </p>
-      </div>
-    </div>
+    <Link
+      href={`/blog/${props.slug}`}
+      className="lg:w-full w-80 px-1 h-full flex flex-col lg:flex-row justify-start items-start lg:justify-between lg:items-center rounded-md shadow-md bg-cod-gray-200 dark:bg-cod-gray-800"
+    >
+      <h3 className="max-w-max font-sans text-lg font-semibold py-1 my-2 text-black dark:text-white">
+        {props.title}
+      </h3>
+      <p className="max-w-max h-full flex flex-row justify-center items-center font-sans text-sm font-normal py-1 text-cod-gray-900 dark:text-cod-gray-50">
+        {readingTime(props.body)} • {props.date}
+      </p>
+    </Link>
   )
 }
