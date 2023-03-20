22dbfc04e15405b4d3adf8b43a975f8123908cf6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 17:08:35 -0300

    refactor: changes to `ProjectCard` key and container grid

--- src/components/ProjectsContainer/index.tsx
+++ src/components/ProjectsContainer/index.tsx
@@ -3,9 +3,9 @@ import { Project, ProjectsContainerProps } from "@types"
 
 export function ProjectsContainer(props: ProjectsContainerProps): JSX.Element {
   return (
-    <div className="w-full min-h-max grid grid-cols-1 lg:grid-cols-2 gap-8 my-2 py-4">
+    <div className="w-full min-h-max grid grid-cols-1 gap-8 my-2 py-4">
       {props.projects.map((project: Project) => (
-        <ProjectCard key={project.name} {...project} />
+        <ProjectCard key={project.title} {...project} />
       ))}
     </div>
   )
