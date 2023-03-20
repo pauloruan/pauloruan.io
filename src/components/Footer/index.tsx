11b90fa3a10cc477fa228c5fc857e2e39c57060d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 16:47:03 -0300

    perf: changes to `Footer` style and content

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,14 @@
-import { SocialContent } from "@components/SocialContent"
-
 export function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear()
+
   return (
-    <footer className="box-border w-full h-12 flex flex-row flex-nowrap items-center justify-between text-center px-2 md:px-32 font-sans">
-      <p className="text-gray-600 dark:text-gray-400 font-sans font-normal text-sm justify-center text-center md:text-base">
+    <footer className="box-border w-full md:w-[80vw] h-12 flex flex-col flex-nowrap items-start justify-center text-center font-sans px-1 sm:px-8 md:px-24 lg:px-36">
+      <p className="max-w-max font-sans text-sm font-normal text-left text-cod-gray-800 dark:text-cod-gray-200">
         &copy; {currentYear} • Paulo Ruan
       </p>
-      <SocialContent />
+      <p className="max-w-max font-sans text-sm font-normal text-left text-cod-gray-800 dark:text-cod-gray-200">
+        Desenvolvido com Next.js & Tailwind CSS.
+      </p>
     </footer>
   )
 }
