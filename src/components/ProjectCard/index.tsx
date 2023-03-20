0ae1863922d98119a54e56b47066221a657c7a76
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 18:57:45 -0300

    refactor: `ProjectCard` component styling changes

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -8,25 +8,26 @@ export function ProjectCard(props: Project): JSX.Element {
   const builder = urlBuilder(client)
 
   return (
-    <div className="lg:w-full w-80 h-full flex flex-col lg:flex-row items-start justify-start gap-4 rounded-md shadow-md bg-cod-gray-200 dark:bg-cod-gray-800">
-      <Link href={`/projetos/${props.slug}`} target="_self">
-        <Image
-          src={builder.image(props.poster).url()}
-          alt={props.title}
-          width={320}
-          height={288}
-          priority
-          className="object-cover rounded-t-md lg:rounded-r-none lg:rounded-l-md"
-        />
-      </Link>
+    <Link
+      href={`/projetos/${props.slug}`}
+      className="lg:w-full w-80 h-full flex flex-col lg:flex-row items-start justify-start gap-4 rounded-md shadow-md bg-cod-gray-200 dark:bg-cod-gray-800"
+    >
+      <Image
+        src={builder.image(props.poster).url()}
+        alt={props.title}
+        width={320}
+        height={288}
+        priority
+        className="object-cover rounded-t-md lg:rounded-r-none lg:rounded-l-md"
+      />
       <div className="flex flex-col flex-wrap gap-1 mx-2 w-full">
         <h3 className="max-w-max font-sans text-xl font-semibold text-left py-1 my-1 text-cod-gray-900 dark:text-cod-gray-50">
           {props.title}
         </h3>
-        <p className="max-w-max font-sans text-base font-normal text-left py-1 my-2 text-cod-gray-900 dark:text-cod-gray-50">
+        <p className="max-w-max font-sans text-sm font-normal text-left py-1 my-2 text-cod-gray-900 dark:text-cod-gray-50">
           {props.summary}
         </p>
       </div>
-    </div>
+    </Link>
   )
 }
