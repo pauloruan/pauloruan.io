aa4644d4793518e6f078a6f24bfb7481d8e18d7a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 17:01:52 -0300

    refactor: `MenuItem` style changes

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 
 export function MenuItem(props: NavLink): JSX.Element {
-  const general = "text-gray-600 dark:text-gray-400 font-medium"
+  const general = "text-cod-gray-800 dark:text-cod-gray-200 font-medium"
   const active = "text-black dark:text-white font-bold"
   const router = useRouter()
   const pathname = `/${router.pathname.split("/")[1]}`
@@ -14,7 +14,7 @@ export function MenuItem(props: NavLink): JSX.Element {
       aria-label={props.name}
       href={props.path}
       target="_self"
-      className={`h-8 w-16 no-underline text-sm md:text-base cursor-pointer p-1 mx-1 rounded-sm flex items-center justify-center transition duration-300 ease-in-out ${style}`}
+      className={`h-8 w-16 no-underline text-sm lg:text-base cursor-pointer p-1 mx-1 flex items-center justify-center transition duration-300 ease-in-out ${style}`}
     >
       {props.name}
     </Link>
