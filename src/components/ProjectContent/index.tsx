1d05b84505f0fdb06b7f4fe49f2e130b5889245d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 17:34:31 -0300

    refactor: icon and image style changes and date rendering

--- src/components/ProjectContent/index.tsx
+++ src/components/ProjectContent/index.tsx
@@ -8,31 +8,37 @@ import { TbWorld } from "react-icons/tb"
 
 export function ProjectContent(props: ProjectContentProps): JSX.Element {
   const builder = urlBuilder(client)
+  const date = new Date(props.project.date).getFullYear().toString()
+
   return (
     <div className="w-full h-full">
-      <div className="w-72 flex flex-row justify-between items-center my-1 py-1 text-gray-600 dark:text-gray-400">
-        <p>2022</p>
+      <div className="w-72 flex flex-row justify-between items-center my-1 py-1 text-cod-gray-800 dark:text-cod-gray-200">
+        <p className="font-sans text-base font-normal text-left py-1 text-cod-gray-900 dark:text-cod-gray-50">
+          {date}
+        </p>
         <div className="w-52 flex flex-row justify-between items-center">
           <Link
             href={props.project.repository}
             target="_blank"
-            className="flex flex-row items-center text-center no-underline hover:underline decoration-2 text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
+            className="flex flex-row items-center text-center no-underline hover:underline decoration-2 text-cod-gray-800 dark:text-cod-gray-200 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
           >
-            <FaCodeBranch className="mr-1" /> Repositório
+            <FaCodeBranch className="mr-2 h-4 w-4 dark:text-white text-black" />
+            Repositório
           </Link>
           <Link
             href={props.project.website}
             target="_blank"
-            className="flex flex-row items-center text-center no-underline hover:underline decoration-2 text-gray-600 dark:text-gray-400 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
+            className="flex flex-row items-center text-center no-underline hover:underline decoration-2 text-cod-gray-800 dark:text-cod-gray-200 hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
           >
-            <TbWorld className="mr-1" /> Website
+            <TbWorld className="mr-2 h-4 w-4 dark:text-white text-black" />
+            Website
           </Link>
         </div>
       </div>
-      <p className="text-black dark:text-white my-2 py-2">
+      <p className="w-full h-full font-sans text-base font-normal text-left py-1 text-cod-gray-900 dark:text-cod-gray-50">
         {props.project.description}
       </p>
-      <ul className="w-full h-full grid grid-cols-1 gap-4 my-4">
+      <ul className="w-full h-full grid grid-cols-1 gap-8 my-8">
         {props.project.images.map((image) => (
           <Image
             key={image._key}
@@ -40,7 +46,8 @@ export function ProjectContent(props: ProjectContentProps): JSX.Element {
             alt={image.alt}
             width={1200}
             height={800}
-            className="rounded-2xl object-cover"
+            priority
+            className="rounded-md object-cover"
           />
         ))}
       </ul>
