f793bb538c4b3ac9b0c082de2eca3cb0450aa16c
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-20 18:21:58 -0300

    refactor: `PostContainer` component styling changes

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -3,13 +3,12 @@ import { PortableText, PortableTextReactComponents } from "@portabletext/react"
 import type { PostProps } from "@types"
 import { readingTime } from "@utils/readTime"
 import Link from "next/link"
-import { RiArrowLeftUpLine } from "react-icons/ri"
 
 export const components: Partial<PortableTextReactComponents> = {
   block: {
     p: ({ children }) => {
       return (
-        <p className="text-black dark:text-white font-normal text-base">
+        <p className="text-base font-normal text-cod-gray-900 dark:text-cod-gray-50">
           {children}
         </p>
       )
@@ -21,7 +20,7 @@ export const components: Partial<PortableTextReactComponents> = {
         <Link
           href={value.href}
           target="_blank"
-          className="hover:underline decoration-2 transition duration-300 ease-in-out text-black dark:text-white font-bold"
+          className="hover:underline decoration-2 transition duration-300 ease-in-out font-semibold text-cod-gray-900 dark:text-cod-gray-50 hover:text-black dark:hover:text-white"
         >
           {children}
         </Link>
@@ -32,41 +31,40 @@ export const components: Partial<PortableTextReactComponents> = {
 
 export function PostContainer(props: PostProps): JSX.Element {
   return (
-    <section className="w-full min-h-[50vh] py-1 px-8 md:py-4 md:px-32 flex flex-col items-center justify-center gap-1">
-      <div className="max-w-[800px] flex flex-col items-center justify-center py-1 px-4 md:py-4 md:px-16 my-1 md:my-4">
-        <div className="w-full flex flex-col my-2 justify-center items-start">
-          <h1 className="w-full text-black dark:text-white font-sans text-5xl font-extrabold text-left py-1">
-            {props.post.title}
-          </h1>
-        </div>
-        <div className="w-full flex flex-col justify-center items-start my-1">
-          <p className="text-gray-600 dark:text-gray-400 font-light text-xs md:text-sm">
-            {props.post.date}
-          </p>
-          <p className="text-gray-600 dark:text-gray-400 font-light text-xs md:text-sm">
-            {readingTime(props.post.body)}
-          </p>
-        </div>
-        <div className="flex flex-col my-1 py-8 justify-center items-start text-start text-black dark:text-white border-b border-b-gray-600 dark:border-b-gray-400 ">
-          <PortableText
-            value={props.post.body}
-            components={components}
-          ></PortableText>
-        </div>
-        <div className="flex flex-col w-full my-4 py-1 items-start text-start justify-center">
-          <FormContact />
-        </div>
-        <div className="flex flex-col w-full my-2 py-1 justify-center items-start text-start">
-          <p className="text-black dark:text-white font-normal text-sm text-start md:text-base my-1 py-1">
-            <Link
-              href="/blog"
-              className="flex flex-row items-center text-center no-underline hover:underline decoration-2 transition duration-300 ease-in-out text-black dark:text-white"
+    <section className="box-border w-full md:w-[80vw] min-h-[50vh] flex flex-col items-start justify-center my-1 md:my-4 px-1 sm:px-8 md:px-24 lg:px-36">
+      <div className="w-full flex flex-col my-2 justify-center items-start">
+        <h1 className="max-w-max h-full font-sans text-4xl font-black text-left my-1 py-1 text-black dark:text-white">
+          {props.post.title}
+        </h1>
+      </div>
+      <div className="w-full flex flex-col justify-center items-start my-1">
+        <p className="max-w-max font-sans text-sm font-normal text-left text-cod-gray-800 dark:text-cod-gray-200">
+          {props.post.date}
+        </p>
+        <p className="max-w-max font-sans text-sm font-normal text-left text-cod-gray-800 dark:text-cod-gray-200">
+          {readingTime(props.post.body)}
+        </p>
+      </div>
+      <div className="flex flex-col my-1 py-8 justify-center items-start text-start text-black dark:text-white border-b border-b-gray-600 dark:border-b-gray-400">
+        <PortableText
+          value={props.post.body}
+          components={components}
+        ></PortableText>
+      </div>
+      <div className="flex flex-row py-2 my-2 justify-start items-center text-start gap-8">
+        {props.post.categories.map((category) => {
+          return (
+            <p
+              key={category}
+              className="flex flex-row items-center justify-center text-center text-xs md:text-sm text-black dark:text-white bg-cod-gray-200 dark:bg-cod-gray-800 px-2 py-2 rounded-sm font-semibold max-h-max max-w-max"
             >
-              <RiArrowLeftUpLine className="mr-1" /> Voltar para a&nbsp;
-              <strong>lista de posts</strong>.
-            </Link>
-          </p>
-        </div>
+              #{category.toLocaleLowerCase()}
+            </p>
+          )
+        })}
+      </div>
+      <div className="flex flex-col w-full my-4 py-1 items-start text-start justify-center">
+        <FormContact />
       </div>
     </section>
   )
