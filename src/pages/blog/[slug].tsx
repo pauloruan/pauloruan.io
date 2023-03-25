e942e1b25a66e53d08f24d972ab77a04f49526f1
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-25 13:12:37 -0300

    perf: `Blog` page SEO configured

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -6,6 +6,7 @@ import { client } from "@lib/sanity.client"
 import { FormattedPost, PostProps } from "@types"
 import { convertToBrazilianDate } from "@utils/convertToBrazilianDate"
 import { sanityQueries } from "@utils/sanityQueries"
+import { NextSeo } from "next-seo"
 import { useRouter } from "next/router"
 
 export async function getStaticProps(context: any) {
@@ -44,6 +45,25 @@ export default function Post(props: PostProps): JSX.Element {
 
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <NextSeo
+        title={`${props.post.title} - Paulo Ruan`}
+        description={props.post.subtitle}
+        canonical={`https://pauloruan.vercel.app/blog/${props.post.slug}`}
+        openGraph={{
+          url: `https://pauloruan.vercel.app/blog/${props.post.slug}`,
+          title: `${props.post.title} - Paulo Ruan`,
+          description: props.post.subtitle,
+          images: [
+            {
+              url: "https://pauloruan.vercel.app/og-image.png",
+              width: 1200,
+              height: 630,
+              alt: `${props.post.title} - Paulo Ruan`
+            }
+          ],
+          site_name: `${props.post.title} - Paulo Ruan`
+        }}
+      />
       <Header />
       <PostContainer post={props.post} />
       <Footer />
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -7,6 +7,7 @@ import { client } from "@lib/sanity.client"
 import { BlogGetStaticProps, BlogProps, FormattedPost } from "@types"
 import { convertToBrazilianDate } from "@utils/convertToBrazilianDate"
 import { sanityQueries } from "@utils/sanityQueries"
+import { NextSeo } from "next-seo"
 import * as React from "react"
 
 export async function getStaticProps(): Promise<BlogGetStaticProps> {
@@ -33,6 +34,26 @@ export default function Blog(props: BlogProps): JSX.Element {
 
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <NextSeo
+        title="Blog - Paulo Ruan"
+        description="Blog de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software."
+        canonical="https://pauloruan.vercel.app/blog"
+        openGraph={{
+          url: "https://pauloruan.vercel.app/blog",
+          title: "Blog - Paulo Ruan",
+          description:
+            "Blog de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software.",
+          images: [
+            {
+              url: "https://pauloruan.vercel.app/og-image.png",
+              width: 1200,
+              height: 630,
+              alt: "Blog - Paulo Ruan"
+            }
+          ],
+          site_name: "Blog - Paulo Ruan"
+        }}
+      />
       <Header />
       <SectionContainer title="Blog">
         <BlogContent />
