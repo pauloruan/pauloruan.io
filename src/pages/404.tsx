420f56bf23dbae8d66d838f0cb9902161095b6be
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-03-25 13:08:26 -0300

    perf: `About` page SEO configured

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,10 +2,31 @@ import { Footer } from "@components/Footer"
 import { Header } from "@components/Header"
 import { NotFoundContent } from "@components/NotFoundContent"
 import { SectionContainer } from "@components/SectionContainer"
+import { NextSeo } from "next-seo"
 
 export default function NotFound(): JSX.Element {
   return (
     <div className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16">
+      <NextSeo
+        title="Página não encontrada - Paulo Ruan"
+        description="Portfólio de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software."
+        canonical="https://pauloruan.vercel.app/404"
+        openGraph={{
+          url: "https://pauloruan.vercel.app/404",
+          title: "Página não encontrada - Paulo Ruan",
+          description:
+            "Portfólio de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software.",
+          images: [
+            {
+              url: "https://pauloruan.vercel.app/og-image.png",
+              width: 1200,
+              height: 630,
+              alt: "Página não encontrada - Paulo Ruan"
+            }
+          ],
+          site_name: "Página não encontrada - Paulo Ruan"
+        }}
+      />
       <Header />
       <SectionContainer title="Página não encontrada">
         <NotFoundContent />
