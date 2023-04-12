a03c3641b06c33d0a12c35bbac4d5538f367892d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-12 17:01:12 -0300

    feat: `RecentBuilding` component created

---
+++ src/components/RecentBuilding/index.tsx
@@ -0,0 +1,23 @@
+import { RecentCard } from "@components/RecentCard"
+import { GlobalContext } from "@contexts/GlobalContext"
+import * as React from "react"
+
+export function RecentBuilding(): JSX.Element {
+  const { projects } = React.useContext(GlobalContext)
+  return (
+    <div className="w-full flex flex-col flex-wrap my-2 lg:my-4">
+      <div className="w-full flex my-1 flex-col items-start">
+        <h2 className="max-w-max h-full font-sans text-lg font-bold text-left text-cod-gray-900 dark:text-cod-gray-50">
+          Construindo
+        </h2>
+      </div>
+      <div className="w-full grid grid-cols-1 gap-4 my-1">
+        {projects
+          .filter((project, index) => !project.published && index < 3)
+          .map((post) => (
+            <RecentCard key={post.title} href="#" {...post} />
+          ))}
+      </div>
+    </div>
+  )
+}
