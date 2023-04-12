570b3e7372d4fabb9e63870db28a747503410d35
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-12 18:40:59 -0300

    refactor: filter posts by published status

--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -4,9 +4,11 @@ import { FormattedPost, PostsListProps } from "@types"
 export function PostsList({ posts }: PostsListProps): JSX.Element {
   return (
     <ul className="w-full min-h-[221px] my-2 py-2 flex flex-col">
-      {posts.map((post: FormattedPost) => (
-        <PostCard key={post.slug} {...post} />
-      ))}
+      {posts
+        .filter((post: FormattedPost) => post.published)
+        .map((post: FormattedPost) => (
+          <PostCard key={post.slug} {...post} />
+        ))}
     </ul>
   )
 }
