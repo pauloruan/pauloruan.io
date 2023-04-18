34720d369b1db76ddb8dbbea967bcdbae5fb8f19
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-18 17:54:06 -0300

    refactor: Posts rendered according to context and search result

--- src/components/BlogContent/index.tsx
+++ src/components/BlogContent/index.tsx
@@ -1,15 +1,24 @@
 import { NoPosts } from "@components/NoPosts"
 import { PostsList } from "@components/PostsList"
+import { SearchPost } from "@components/SearchPost"
 import { GlobalContext } from "@contexts/GlobalContext"
+import { FormattedPost } from "@types"
 import * as React from "react"
 
 export function BlogContent(): JSX.Element {
-  const { posts } = React.useContext(GlobalContext)
+  const { posts, searchValue } = React.useContext(GlobalContext)
+  const initialDisplayPosts = posts
+  const filteredPosts = searchValue
+    ? initialDisplayPosts.filter((post: FormattedPost) => {
+        return post.title.toLowerCase().includes(searchValue.toLowerCase())
+      })
+    : posts
 
   return (
     <div className="w-full flex flex-col justify-between items-start min-h-[280px]">
-      {posts.length === 0 && <NoPosts />}
-      {posts.length > 0 && <PostsList posts={posts} />}
+      <SearchPost />
+      {filteredPosts.length === 0 && <NoPosts />}
+      {filteredPosts.length > 0 && <PostsList posts={filteredPosts} />}
     </div>
   )
 }
