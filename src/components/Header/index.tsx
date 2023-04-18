2239ac354a1b841f49cc3b104510330328b1dffc
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-18 17:48:19 -0300

    refactor: `LogoLink` component rendered in `Header`

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import { ButtonToggleTheme } from "@components/ButtonToggleTheme"
+import { LogoLink } from "@components/LogoLink"
 import { Menu } from "@components/Menu"
 import { navLinks } from "@utils/navLinks"
 
 export function Header(): JSX.Element {
   return (
     <header className="box-border w-full md:w-[80vw] h-12 flex flex-row flex-nowrap items-center justify-between px-1 sm:px-8 md:px-24 lg:px-36">
+      <LogoLink />
       <Menu links={navLinks} />
       <div className="flex flex-row justify-between items-center text-center gap-2">
         <ButtonToggleTheme />
