b85bbc0452e82f12f21c69e242c397b6b616d026
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-18 18:50:48 -0300

    perf: Icon size changes

--- src/components/SearchPost/index.tsx
+++ src/components/SearchPost/index.tsx
@@ -12,7 +12,7 @@ export function SearchPost(): JSX.Element {
 
   return (
     <div className="w-full h-full flex flex-row flex-nowrap items-center justify-start my-2 px-2 py-1 bg-cod-gray-200 dark:bg-cod-gray-800 rounded-md">
-      <BsSearch className="w-6 h-6 mr-2 text-cod-gray-900 dark:text-cod-gray-50" />
+      <BsSearch className="w-4 h-4 mr-2 text-cod-gray-900 dark:text-cod-gray-50" />
       <input
         type="search"
         className="block w-full rounded-md border-0 bg-transparent px-1 py-2 focus:outline-none focus:ring-0 focus:border-transparent text-cod-gray-900 dark:text-cod-gray-50 placeholder-cod-gray-900 dark:placeholder-cod-gray-50 placeholder:opacity-70"
