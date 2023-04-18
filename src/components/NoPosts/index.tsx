fb75c785b073ac68f897a664254eaccffc6d1f48
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-18 17:46:10 -0300

    perf: Component text style changes

--- src/components/NoPosts/index.tsx
+++ src/components/NoPosts/index.tsx
@@ -1,7 +1,7 @@
 export function NoPosts(): JSX.Element {
   return (
     <div className="w-full min-h-[221px] flex justify-center items-center my-2 py-2">
-      <h3 className="text-gray-600 dark:text-gray-400 font-sans my-1 font-bold">
+      <h3 className="text-black dark:text-white font-sans my-1 font-bold text-xl">
         Nenhum post encontrado.
       </h3>
     </div>
