efddd1815f36f5b3d4ffe58c53844062f6ed64bb
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-29 15:57:08 -0300

    refactor: Unnecessary components removed.

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,11 +1,9 @@
 import { Hero } from "@components/Hero"
-import { RecentGrid } from "@components/RecentGrid"
 
 export function HomeContent(): JSX.Element {
   return (
     <div className="flex flex-col flex-wrap w-full justify-center items-center">
       <Hero />
-      <RecentGrid />
     </div>
   )
 }
