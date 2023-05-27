25330bdc6d8034531621d29ba29b2e4df1b2981b
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-27 16:39:17 -0300

    refactor: Update `Animation` component styles

--- src/components/Animation/index.tsx
+++ src/components/Animation/index.tsx
@@ -7,7 +7,7 @@ export type AnimationProps = {
 export function Animation({ children }: AnimationProps): JSX.Element {
   return (
     <motion.div
-      className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16"
+      className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen flex-col items-center justify-between w-full max-w-[666px] mx-auto"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{
