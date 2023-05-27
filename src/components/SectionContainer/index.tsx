c7729bfad666c8db1370fd1a93fff85c6e8bb96e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-27 16:25:31 -0300

    refactor: Updated `SectionContainer` component to improve responsiveness and layout

--- src/components/SectionContainer/index.tsx
+++ src/components/SectionContainer/index.tsx
@@ -2,7 +2,7 @@ import { SectionContainerProps } from "@types"
 
 export function SectionContainer(props: SectionContainerProps): JSX.Element {
   return (
-    <section className="box-border w-full md:w-[80vw] min-h-[50vh] flex flex-col items-start justify-center my-1 md:my-4 px-1 sm:px-8 md:px-24 lg:px-36">
+    <section className="box-border w-full h-full min-h-[80vh] max-w-[666px] mx-auto flex flex-col items-start justify-center my-2 px-4 py-2">
       {props.title && (
         <h1 className="max-w-max h-full font-sans text-4xl font-black text-left my-1 py-1 text-black dark:text-white">
           {props.title}
@@ -13,7 +13,9 @@ export function SectionContainer(props: SectionContainerProps): JSX.Element {
           {props.subtitle}
         </h2>
       )}
-      <div className="w-full flex flex-col items-start">{props.children}</div>
+      <div className="w-full flex flex-col items-start grow">
+        {props.children}
+      </div>
     </section>
   )
 }
