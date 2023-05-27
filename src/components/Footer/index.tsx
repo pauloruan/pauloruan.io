604c628480addd7049c51485521e327b640fc65d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-27 15:37:41 -0300

    refactor: Update `Footer` component with responsive styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ export function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="box-border w-full md:w-[80vw] h-12 flex flex-col flex-nowrap items-start justify-center text-center font-sans px-1 sm:px-8 md:px-24 lg:px-36">
+    <footer className="box-border w-full max-w-[666px] mx-auto h-12 flex flex-col flex-nowrap items-start justify-center text-center font-sans text-sm px-4 py-2">
       <p className="max-w-max font-sans text-sm font-normal text-left text-cod-gray-800 dark:text-cod-gray-200">
         &copy; {currentYear} • Paulo Ruan
       </p>
