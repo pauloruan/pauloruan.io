4093c8aa0557c4b8e57da6b68ae942d5a9e91317
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-27 17:34:07 -0300

    refactor: Refactor page for improved organization and readability

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -1,12 +1,11 @@
 import { AboutContent } from "@components/AboutContent"
+import { Animation } from "@components/Animation"
 import { Footer } from "@components/Footer"
 import { Header } from "@components/Header"
 import { SectionContainer } from "@components/SectionContainer"
 import { client } from "@lib/sanity.client"
 import { SobreGetStaticProps, SobreProps } from "@types"
 import { sanityQueries } from "@utils/sanityQueries"
-import { motion } from "framer-motion"
-import { NextSeo } from "next-seo"
 
 export async function getStaticProps(): Promise<SobreGetStaticProps> {
   const [about] = await client.fetch(sanityQueries.about)
@@ -18,41 +17,14 @@ export async function getStaticProps(): Promise<SobreGetStaticProps> {
 
 export default function Sobre(props: SobreProps): JSX.Element {
   return (
-    <motion.div
-      className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{
-        duration: 0.5,
-        delay: 0,
-        ease: "easeInOut"
-      }}
-    >
-      <NextSeo
-        title="Sobre Mim - Paulo Ruan"
-        description="Portfólio de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software."
-        canonical="https://pauloruan.vercel.app/sobre"
-        openGraph={{
-          url: "https://pauloruan.vercel.app/sobre",
-          title: "Sobre Mim - Paulo Ruan",
-          description:
-            "Portfólio de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software.",
-          images: [
-            {
-              url: "https://pauloruan.vercel.app/og-image.png",
-              width: 1200,
-              height: 628,
-              alt: "Sobre Mim - Paulo Ruan"
-            }
-          ],
-          site_name: "Sobre Mim - Paulo Ruan"
-        }}
-      />
-      <Header />
-      <SectionContainer title="Sobre Mim">
-        <AboutContent {...props.about} />
-      </SectionContainer>
-      <Footer />
-    </motion.div>
+    <main className="min-h-screen w-full bg-cod-gray-100 dark:bg-cod-gray-900">
+      <Animation>
+        <Header />
+        <SectionContainer title="Sobre Mim">
+          <AboutContent {...props.about} />
+        </SectionContainer>
+        <Footer />
+      </Animation>
+    </main>
   )
 }
