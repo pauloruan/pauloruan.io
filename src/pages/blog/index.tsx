1dc4d5ef0ff5abb1a127a6233c624fa80630feb6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-27 17:31:41 -0300

    refactor: Reorganized the page structure for improved readability

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,3 +1,4 @@
+import { Animation } from "@components/Animation"
 import { BlogContent } from "@components/BlogContent"
 import { Footer } from "@components/Footer"
 import { Header } from "@components/Header"
@@ -7,8 +8,6 @@ import { client } from "@lib/sanity.client"
 import { BlogGetStaticProps, BlogProps, FormattedPost } from "@types"
 import { convertToBrazilianDate } from "@utils/convertToBrazilianDate"
 import { sanityQueries } from "@utils/sanityQueries"
-import { motion } from "framer-motion"
-import { NextSeo } from "next-seo"
 import * as React from "react"
 
 export async function getStaticProps(): Promise<BlogGetStaticProps> {
@@ -34,41 +33,14 @@ export default function Blog(props: BlogProps): JSX.Element {
   })
 
   return (
-    <motion.div
-      className="bg-cod-gray-100 dark:bg-cod-gray-900 flex min-h-screen w-full flex-col items-center justify-between py-2 px-4 md:py-4 md:px-16"
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{
-        duration: 0.3,
-        delay: 0,
-        ease: "easeInOut"
-      }}
-    >
-      <NextSeo
-        title="Blog - Paulo Ruan"
-        description="Blog de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software."
-        canonical="https://pauloruan.vercel.app/blog"
-        openGraph={{
-          url: "https://pauloruan.vercel.app/blog",
-          title: "Blog - Paulo Ruan",
-          description:
-            "Blog de Paulo Ruan, desenvolvedor web e estudante de Engenharia de Software.",
-          images: [
-            {
-              url: "https://pauloruan.vercel.app/og-image.png",
-              width: 1200,
-              height: 628,
-              alt: "Blog - Paulo Ruan"
-            }
-          ],
-          site_name: "Blog - Paulo Ruan"
-        }}
-      />
-      <Header />
-      <SectionContainer title="Blog">
-        <BlogContent />
-      </SectionContainer>
-      <Footer />
-    </motion.div>
+    <main className="min-h-screen w-full bg-cod-gray-100 dark:bg-cod-gray-900">
+      <Animation>
+        <Header />
+        <SectionContainer title="Blog">
+          <BlogContent />
+        </SectionContainer>
+        <Footer />
+      </Animation>
+    </main>
   )
 }
