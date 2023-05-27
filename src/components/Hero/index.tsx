6b2e3603d3df9c4475fcc1c3f4696a108f17b73a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-27 16:57:32 -0300

    refactor: Updated the `Hero` component to include a developer image

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,28 @@
+import Image from "next/image"
 import Link from "next/link"
 
 export function Hero(): JSX.Element {
   return (
-    <div className="flex flex-nowrap flex-col w-full h-full justify-center items-center">
-      <div className="flex flex-col h-full min-w-[30%] w-full justify-start items-start my-1">
-        <h1 className="max-w-max max-h-max font-sans text-2xl font-black my-1 py-1 text-left text-black dark:text-white">
-          Olá, eu sou o Paulo Ruan!
-        </h1>
-        <p className="max-w-max max-h-max font-sans text-base font-normal text-left py-1 text-cod-gray-900 dark:text-cod-gray-50">
+    <div className="flex flex-nowrap flex-col w-full h-full justify-center items-center gap-4">
+      <div className="flex flex-nowrap flex-col md:flex-row w-full h-full justify-start items-start md:justify-between md:items-center gap-2">
+        <Image
+          src="https://avatars.githubusercontent.com/u/85769241?v=4"
+          alt="Paulo Ruan"
+          width={1940}
+          height={1940}
+          className="rounded-full w-24 h-24 my-2 md:mr-4"
+        />
+        <div className="flex flex-nowrap flex-col w-full h-full justify-center items-start text-start">
+          <h1 className="max-w-max max-h-max font-sans text-2xl font-black text-left text-black dark:text-white">
+            Oi, eu sou o Paulo Ruan!
+          </h1>
+          <p className="max-w-max max-h-max font-sans text-sm font-medium text-left text-black dark:text-white">
+            Desenvolvedor Full Stack JR
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-col h-full min-w-[30%] w-full justify-start items-start">
+        <p className="max-w-max max-h-max font-sans text-base font-normal text-left text-cod-gray-900 dark:text-cod-gray-50">
           Bem-vindo(a) ao meu lugar na internet. Eu construo coisas para a web.
           <br />
           Sou apaixonado por tecnologia, desenvolvimento de software e&nbsp;
