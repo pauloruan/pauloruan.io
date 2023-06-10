f6e3e33568f9f20d3392b3adf1ecdec31be494e5
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-06-10 00:16:58 -0300

    perf: `NowPlaying` component rendered.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,17 @@
+import { NowPlaying } from "@components/NowPlaying"
+
 export function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="box-border w-full max-w-[666px] mx-auto h-12 flex flex-col flex-nowrap items-start justify-center text-center font-sans text-xs px-4 py-2">
-      <p className="max-w-max font-sans font-normal text-left text-cod-gray-800 dark:text-cod-gray-200 leading-relaxed tracking-wide">
-        &copy; <time>{currentYear}</time> • Paulo Ruan
-      </p>
-      <p className="max-w-max font-sans font-normal text-left text-cod-gray-800 dark:text-cod-gray-200 leading-relaxed tracking-wide">
-        Desenvolvido com Next.js & Tailwind CSS.
-      </p>
+    <footer className="box-border w-full max-w-[666px] mx-auto h-32 flex flex-col flex-nowrap items-start justify-between text-center font-sans text-xs px-4 py-4">
+      <NowPlaying />
+      <div className="max-w-max font-sans font-normal text-left text-cod-gray-800 dark:text-cod-gray-200 leading-relaxed tracking-wide">
+        <p>
+          &copy; <time>{currentYear}</time> • Paulo Ruan
+        </p>
+        <p>Desenvolvido com Next.js & Tailwind CSS.</p>
+      </div>
     </footer>
   )
 }
