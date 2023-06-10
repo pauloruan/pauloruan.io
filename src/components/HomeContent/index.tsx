aca179f5ee4ca00e7a67e641b901996cd7fa3327
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-06-10 00:10:29 -0300

    perf: `FrequentTechnologies` and `RecentPosts` components rendered.

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,9 +1,13 @@
+import { FrequentTechnologies } from "@components/FrequentTechnologies"
 import { Hero } from "@components/Hero"
+import { RecentPosts } from "@components/RecentPosts"
 
 export function HomeContent(): JSX.Element {
   return (
     <div className="flex flex-col flex-wrap w-full justify-center items-center gap-5">
       <Hero />
+      <FrequentTechnologies />
+      <RecentPosts />
     </div>
   )
 }
