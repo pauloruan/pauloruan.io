64e40f90522f7af80e6376865c6ee5345df2723f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-06-09 21:37:06 -0300

    refactor: Style changes applied.

--- src/components/Posts/components/Card.tsx
+++ src/components/Posts/components/Card.tsx
@@ -6,7 +6,7 @@ export function Card(props: Post): JSX.Element {
   return (
     <Link
       href={`/blog/${slug}`}
-      className="w-full h-full flex flex-col justify-center items-start shadow-md transition-all duration-300 ease-in-out cursor-pointer border border-cod-gray-200 dark:border-cod-gray-800 rounded-md px-4 py-2 hover:bg-cod-gray-200 dark:hover:bg-cod-gray-800"
+      className="w-full h-full flex flex-col justify-center items-start shadow-md transition-all duration-300 ease-in-out cursor-pointer rounded-md px-4 py-2 bg-cod-gray-200 dark:bg-cod-gray-800"
     >
       <h3 className="max-w-max font-sans text-lg font-semibold my-1 text-black dark:text-white">
         {title}
