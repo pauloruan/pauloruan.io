0530bed36597a8574ab2a1e92904c84c80c1e0e6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-06-13 17:07:45 -0300

    feat: SEO configuration file created.

---
+++ src/utils/next-seo.config.ts
@@ -0,0 +1,35 @@
+import { DefaultSeoProps } from "next-seo"
+
+const title = "Paulo Ruan"
+const description =
+  "Portfólio de Paulo Ruan Desenvolvedor Web, apaixonado por tecnologia e programação."
+
+const config: DefaultSeoProps = {
+  title,
+  description,
+  canonical: "https://pauloruan.vercel.app",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: "https://pauloruan.vercel.app",
+    siteName: "pauloruan.io",
+    title,
+    description,
+    images: [
+      {
+        url: "https://pauloruan.vercel.app/api/og",
+        width: 1200,
+        height: 628,
+        alt: "Og Image Alt",
+        type: "image/jpeg"
+      }
+    ]
+  },
+  twitter: {
+    handle: "@handle",
+    site: "@site",
+    cardType: "summary_large_image"
+  }
+}
+
+export default config
