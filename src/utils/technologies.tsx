9f381fc6b77cb3aac5675df680130aa691e81d4d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-06-14 18:46:27 -0300

    perf: Icon size changed.

--- src/utils/technologies.tsx
+++ src/utils/technologies.tsx
@@ -13,61 +13,61 @@ export const technologies = [
   {
     url: "https://www.javascript.com",
     icon: (
-      <SiJavascript className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiJavascript className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://www.typescriptlang.org",
     icon: (
-      <SiTypescript className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiTypescript className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://pt-br.reactjs.org",
     icon: (
-      <FaReact className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <FaReact className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://angular.io",
     icon: (
-      <SiAngular className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiAngular className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://nodejs.org",
     icon: (
-      <FaNodeJs className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <FaNodeJs className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://www.java.com/pt-BR",
     icon: (
-      <FaJava className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <FaJava className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://spring.io/projects/spring-boot",
     icon: (
-      <SiSpring className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiSpring className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://www.docker.com",
     icon: (
-      <SiDocker className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiDocker className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://www.postgresql.org",
     icon: (
-      <SiPostgresql className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiPostgresql className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   },
   {
     url: "https://www.mongodb.com",
     icon: (
-      <SiMongodb className="h-6 w-6 text-black dark:text-white text-sm font-medium" />
+      <SiMongodb className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
     )
   }
 ] as Technology[]
