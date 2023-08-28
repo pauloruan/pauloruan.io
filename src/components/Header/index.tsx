684ed236e23cf0c6576c8a73b8289fff7bf82aa3
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-08-28 13:36:03 -0300

    feat: Header style changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { navLinks } from "@utils/navLinks"
 
 export function Header(): JSX.Element {
   return (
-    <header className="box-border w-[96%] max-w-[666px] mx-auto h-12 flex flex-row flex-nowrap items-center justify-between px-4 py-2 my-2 shadow-md rounded-md bg-cod-gray-200 dark:bg-cod-gray-800">
+    <header className="box-border w-[96%] max-w-[666px] mx-auto h-12 flex flex-row flex-nowrap items-center justify-between px-4 py-2 my-2 shadow-md rounded-sm bg-cod-gray-200 dark:bg-cod-gray-800">
       <LogoLink />
       <Menu.Container>
         {navLinks.map((link: NavLink) => (
