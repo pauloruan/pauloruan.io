84167d222626ceba43ad1e3133b139a133028791
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-08-28 13:03:35 -0300

    feat: Removed `FrequentTechnologies` and `RecentPosts` components

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,13 +1,9 @@
-import { FrequentTechnologies } from "@components/FrequentTechnologies"
 import { Hero } from "@components/Hero"
-import { RecentPosts } from "@components/RecentPosts"
 
 export function HomeContent(): JSX.Element {
   return (
     <div className="flex flex-col flex-wrap w-full justify-center items-center gap-5">
       <Hero />
-      <FrequentTechnologies />
-      <RecentPosts />
     </div>
   )
 }
