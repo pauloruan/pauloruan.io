82c0d3f0fc8c5c1b1dcb96ae67f664bee0aefd33
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-11 12:01:32 -0300

    feat: add top border

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="box-border w-full max-w-[666px] mx-auto h-32 flex flex-col flex-nowrap items-start justify-between text-center font-sans text-xs px-4 py-4">
+    <footer className="box-border w-full max-w-[666px] mx-auto h-32 flex flex-col flex-nowrap items-start justify-between text-center font-sans text-xs px-4 py-4 border-0 border-t border-cod-gray-300 dark:border-cod-gray-600">
       <NowPlaying />
       <div className="max-w-max font-sans font-normal text-left text-cod-gray-800 dark:text-cod-gray-200 leading-relaxed tracking-wide">
         <p>
