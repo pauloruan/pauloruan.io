68d1abf77b64effb163b54869f3ffeba76c0f79e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-11 15:28:54 -0300

    refactor: Update styles in Footer, Header, and SectionContainer components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ import { navLinks } from "@utils/navLinks"
 
 export function Header(): JSX.Element {
   return (
-    <header className="box-border w-full max-w-[666px] mx-auto h-12 flex flex-col items-center justify-center border-0 border-b border-cod-gray-300 dark:border-cod-gray-600">
+    <header className="box-border w-full max-w-[666px] mx-auto my-2 h-12 flex flex-col items-center justify-center border-0 border-b border-cod-gray-300 dark:border-cod-gray-600">
       <div className="w-[96%] h-full px-2 py-2 flex flex-row flex-nowrap items-center justify-between">
         <LogoLink />
         <Menu.Container>
--- src/components/SectionContainer/index.tsx
+++ src/components/SectionContainer/index.tsx
@@ -2,7 +2,7 @@ import { ScrollToTopButton } from "@components/ScrollToTopButton"
 
 export function SectionContainer(props: SectionContainerProps): JSX.Element {
   return (
-    <section className="box-border w-full h-full min-h-[80vh] max-w-[666px] mx-auto flex flex-col items-start justify-center my-2 px-4 py-2">
+    <section className="box-border w-full h-full min-h-[70vh] max-w-[666px] mx-auto flex flex-col items-start justify-center my-2 px-4 py-2">
       {props.title && (
         <h1 className="max-w-max h-full font-sans text-3xl font-black text-left py-1 text-black dark:text-white leading-relaxed tracking-tight">
           {props.title}
@@ -13,7 +13,7 @@ export function SectionContainer(props: SectionContainerProps): JSX.Element {
           {props.subtitle}
         </h2>
       )}
-      <div className="min-h-[76vh] h-full w-full flex flex-col items-start">
+      <div className="min-h-[60vh] h-full w-full flex flex-col items-start">
         {props.children}
       </div>
       <ScrollToTopButton />
