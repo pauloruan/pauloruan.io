122d7c697f532dab0bd2b069c94186173345d072
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-11 12:09:30 -0300

    feat: adjust content width

--- src/components/Menu/components/Buttons.tsx
+++ src/components/Menu/components/Buttons.tsx
@@ -1,6 +1,6 @@
 export function Buttons({ children }: MenuProps): JSX.Element {
   return (
-    <div className="flex flex-row max-w-max h-full justify-between items-center text-center gap-2 before:content-[''] before:w-[2px] before:h-[50%] md:before:h-[60%] before:bg-cod-gray-800/50 dark:before:bg-cod-gray-200/50">
+    <div className="flex flex-row max-w-max h-full justify-between items-center text-center gap-2 before:content-[''] before:w-[1px] before:h-[50%] md:before:h-[60%] before:bg-cod-gray-800/50 dark:before:bg-cod-gray-200/50">
       {children}
     </div>
   )
