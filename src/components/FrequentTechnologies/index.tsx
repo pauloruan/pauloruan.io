70521a1cf969d31bce94aba7bb3ae7a83099d9cf
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-12 22:59:37 -0300

    refactor: Refactor FrequentTechnologies component to display ordered technologies

--- src/components/FrequentTechnologies/index.tsx
+++ src/components/FrequentTechnologies/index.tsx
@@ -2,16 +2,19 @@ import { TechCard } from "@components/TechCard"
 import { technologies } from "@utils/technologies"
 
 export function FrequentTechnologies() {
+  const orderedTechnologies = technologies
+    .slice()
+    .filter(({ frequentlyUsed }) => frequentlyUsed)
+    .sort((a, b) => a.order - b.order)
+
   return (
     <div className="w-full h-full flex flex-col justify-center items-start my-1 py-1 space-y-1">
       <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-black dark:text-white font-sans my-1 py-1">
         Tecnologias que uso com frequência
       </h3>
-      <div className="w-full h-full grid grid-rows-1 grid-cols-10 gap-1">
-        {technologies.map(({ url, icon: Icon }: Technology) => (
-          <TechCard key={url} url={url}>
-            <Icon className="h-8 w-8 text-black dark:text-white text-sm font-medium" />
-          </TechCard>
+      <div className="w-full h-full flex flex-row flex-wrap justify-start items-center gap-1">
+        {orderedTechnologies.map((tech: ITechnology) => (
+          <TechCard key={tech.slug} {...tech} />
         ))}
       </div>
     </div>
