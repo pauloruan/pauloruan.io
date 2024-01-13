3194f87db127a7d3e15f64b4d5fb141b63db68e6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-01-12 22:57:05 -0300

    feat: Add FrequentTechnologies component to HomeContent

--- src/components/HomeContent/index.tsx
+++ src/components/HomeContent/index.tsx
@@ -1,9 +1,11 @@
+import { FrequentTechnologies } from "@components/FrequentTechnologies"
 import { Hero } from "@components/Hero"
 
 export function HomeContent(): JSX.Element {
   return (
     <div className="flex flex-col flex-wrap w-full justify-center items-center gap-5">
       <Hero />
+      <FrequentTechnologies />
     </div>
   )
 }
