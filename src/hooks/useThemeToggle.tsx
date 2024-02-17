9f2c5b98e08c490562284f08290711883d8094b8
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:19:51 -0300

    feat: Add useThemeToggle hook for theme toggling

---
+++ src/hooks/useThemeToggle.tsx
@@ -0,0 +1,17 @@
+import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
+
+export function useThemeToggle() {
+  const { theme, setTheme, systemTheme } = useTheme()
+  const [currentTheme, setCurrentTheme] = useState<string | undefined>("")
+
+  useEffect(() => {
+    setCurrentTheme(theme === "system" ? systemTheme : theme)
+  }, [systemTheme, theme])
+
+  function toggleTheme(): void {
+    setTheme(currentTheme === "dark" ? "light" : "dark")
+  }
+
+  return { currentTheme, toggleTheme }
+}
