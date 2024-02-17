29f201c78d7d3ddbaa0bb39da57ff1bddfdbe0c6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:27:18 -0300

    refactor: Update Header component

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,22 +1,18 @@
-import { ButtonToggleTheme } from "@/components/shared/Header/ButtonToggleTheme"
-import { LogoLink } from "@/components/shared/Header/LogoLink"
 import { Menu } from "@/components/shared/Header/Menu"
+import { ThemeToggleButton } from "@/components/shared/Header/ThemeToggleButton"
 import { navLinks } from "@/utils/navLinks"
 
 export function Header(): JSX.Element {
   return (
-    <header className="box-border w-full max-w-[666px] mx-auto my-2 h-12 flex flex-col items-center justify-center border-0 border-b border-cod-gray-300 dark:border-cod-gray-600">
-      <div className="w-[96%] h-full px-2 py-2 flex flex-row flex-nowrap items-center justify-between">
-        <LogoLink />
-        <Menu.Container>
-          {navLinks.map((link: INavLink) => (
-            <Menu.Item key={link.path} {...link} />
-          ))}
-        </Menu.Container>
+    <header className="box-border w-full max-w-[666px] mx-auto my-2 h-12 flex flex-col items-center justify-center">
+      <Menu.Container>
+        <Menu.Nav>
+          <Menu.Links links={navLinks} />
+        </Menu.Nav>
         <Menu.Buttons>
-          <ButtonToggleTheme />
+          <ThemeToggleButton />
         </Menu.Buttons>
-      </div>
+      </Menu.Container>
     </header>
   )
 }
