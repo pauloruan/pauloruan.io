38e7d5988cf1ecc4ef3e9ae43b3015d28358063d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:25:41 -0300

    refactor: Update import of useRouter to usePathname in Item.tsx

--- src/components/shared/Header/Menu/components/Item.tsx
+++ src/components/shared/Header/Menu/components/Item.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link"
-import { useRouter } from "next/router"
+import { usePathname } from "next/navigation"
 
 export function Item({ name, path }: INavLink): JSX.Element {
-  const router = useRouter()
-  const active = "text-black dark:text-white font-bold"
-  const general = "text-cod-gray-800 dark:text-cod-gray-200 font-medium"
-  const pathname = `/${router.pathname.split("/")[1]}`
-  const style = pathname === path ? active : general
+  const pathname = usePathname()
+  const currentPath = pathname === path ? true : false
 
   return (
     <Link
-      aria-label={name}
       href={path}
       target="_self"
-      className={`h-8 w-16 no-underline text-sm cursor-pointer p-1 mx-1 flex items-center justify-center transition duration-300 ease-in-out ${style}`}
+      aria-label={name}
+      data-current-path={currentPath}
+      className="h-8 px-1 max-w-max no-underline text-sm md:text-base text-primary dark:text-primary font-medium cursor-pointer flex items-center justify-center text-center transition duration-300 ease-in-out data-[current-path='true']:font-semibold data-[current-path='true']:underline data-[current-path='true']:underline-offset-2 data-[current-path='true']:decoration-accent"
     >
       {name}
     </Link>
