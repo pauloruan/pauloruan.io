46993ad87b077c2fd8690be3e698ea28fcb14d3d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:51:37 -0300

    chore: Update frequent technologies list

--- src/components/shared/FrequentTechnologies/index.tsx
+++ src/components/shared/FrequentTechnologies/index.tsx
@@ -8,13 +8,13 @@ export function FrequentTechnologies() {
     .sort((a, b) => a.order - b.order)
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-start my-1 py-1 space-y-1">
-      <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-black dark:text-white font-sans my-1 py-1">
-        Tecnologias que uso com frequência
+    <div className="w-full h-full flex flex-col justify-center items-start my-1 py-2 px-2 space-y-2">
+      <h3 className="text-lg md:text-xl max-w-max max-h-max my-1 py-1 font-sans font-semibold text-black dark:text-white">
+        Tecnologias utilizadas frequentemente
       </h3>
       <div className="w-full h-full flex flex-row flex-wrap justify-start items-center gap-1">
         {orderedTechnologies.map((tech: ITechnology) => (
-          <LinkCard key={tech.slug} linkData={tech} />
+          <LinkCard key={tech.slug} data={tech} />
         ))}
       </div>
     </div>
