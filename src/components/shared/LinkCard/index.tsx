8d88b58aee318e49ce791d0ab4c4837f8974d28d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:50:10 -0300

    refactor: Update LinkCard component to use "data" property instead of "linkData"

--- src/components/shared/LinkCard/index.tsx
+++ src/components/shared/LinkCard/index.tsx
@@ -1,16 +1,16 @@
 import Link from "next/link"
 
-export function LinkCard({ linkData }: ILinkCardProps): JSX.Element {
-  const { url, name, renderLabel, label, icon: Icon } = linkData
+export function LinkCard({ data }: ILinkCardProps): JSX.Element {
+  const { url, name, renderLabel, label, icon: Icon } = data
 
   return (
     <Link
       href={url}
       passHref
       target="_blank"
-      className="p-2 max-h-max max-w-max flex flex-row items-center justify-center text-center gap-1 text-black dark:text-white text-sm font-normal transition-all duration-300 ease-in-out hover:text-black/60 dark:hover:text-white/60 hover:underline hover:underline-offset-2 group"
+      className="p-2 max-h-max max-w-max flex flex-row items-center justify-center text-center gap-1 text-primary dark:text-primary font-sans text-sm font-normal transition-all duration-300 ease-in-out group rounded-md bg-card dark:bg-card hover:underline cursor-pointer shadow-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent dark:hover:text-accent-foreground"
     >
-      <Icon className="h-4 w-4 text-black dark:text-white text-sm font-medium transition-all duration-300 ease-in-out group-hover:text-black/60 dark:group-hover:text-white/60" />
+      <Icon className="h-4 w-4 text-primary dark:text-primary text-sm font-medium transition-all duration-300 ease-in-out group-hover:text-accent-foreground dark:group-hover:text-accent-foreground" />
       {renderLabel ? label : name}
     </Link>
   )
