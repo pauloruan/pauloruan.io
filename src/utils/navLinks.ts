a7f1f7ef7c13e5aa8a590ec0d160b5f9b57d5f40
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 00:00:06 -0300

    feat: Add links to "Home" and "Snippets" in the navigation menu

--- src/utils/navLinks.ts
+++ src/utils/navLinks.ts
@@ -1,10 +1,18 @@
 export const navLinks: INavLink[] = [
   {
+    name: "Home",
+    path: "/"
+  },
+  {
     name: "Sobre",
     path: "/sobre"
   },
   {
     name: "Blog",
     path: "/blog"
+  },
+  {
+    name: "Snippets",
+    path: "/snippets"
   }
 ]
