e53b0fce3dbaee93a5fd319d01ce2133c4c11654
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:45:08 -0300

    feat: Add Contact component

---
+++ src/components/shared/Contact/index.tsx
@@ -0,0 +1,21 @@
+import { SocialMediaList } from "../SocialMediaList"
+
+export function Contact() {
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-start my-1 py-2 px-2 space-y-2 bg-yellow-300">
+      <h3 className="text-lg md:text-xl max-w-max max-h-max font-semibold text-black dark:text-white font-sans my-1">
+        Contato
+      </h3>
+      <p className="max-w-max max-h-max my-2 font-sans text-sm md:text-base font-normal text-left text-theme-dark dark:text-theme-light">
+        Estou sempre aberto a conversar, conectar-me com pessoas inovadoras e
+        explorar novas oportunidades. Se você tem um projeto ou ideia em mente
+        no qual posso contribuir, não hesite em entrar em contato. Vamos
+        discutir como transformar suas ideias em experiências.
+      </p>
+      <p className="max-w-max max-h-max my-2 font-sans text-sm md:text-base font-normal text-left text-theme-dark dark:text-theme-light">
+        Você pode me encontrar em qualquer uma das redes sociais abaixo
+      </p>
+      <SocialMediaList />
+    </div>
+  )
+}
