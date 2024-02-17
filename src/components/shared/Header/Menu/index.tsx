364ed3a82a347b7043355b7ae1ba6bd78e69be23
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-17 13:24:31 -0300

    feat: Add Links and Nav components to Menu

--- src/components/shared/Header/Menu/index.tsx
+++ src/components/shared/Header/Menu/index.tsx
@@ -1,9 +1,13 @@
 import { Buttons } from "./components/Buttons"
 import { Container } from "./components/Container"
 import { Item } from "./components/Item"
+import { Links } from "./components/Links"
+import { Nav } from "./components/Nav"
 
 export const Menu = {
   Buttons,
   Container,
-  Item
+  Item,
+  Nav,
+  Links
 }
