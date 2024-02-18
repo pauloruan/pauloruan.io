338b44fe01c581577d884816beccefce5d123e4f
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-18 16:29:36 -0300

    refactor: Update property type in Container.tsx to IPostsProps

--- src/components/Posts/components/Container.tsx
+++ src/components/Posts/components/Container.tsx
@@ -1,4 +1,4 @@
-export function Container({ children }: PostsProps): JSX.Element {
+export function Container({ children }: IPostsProps): JSX.Element {
   return (
     <div className="w-full h-full flex flex-col justify-between items-start min-h-[280px]">
       {children}
