8f38af6f503015efd68cad344d3d6956a83b7f7e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-18 16:30:37 -0300

    feat: Update Posts components

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -2,12 +2,14 @@ import { Card } from "./components/Card"
 import { Container } from "./components/Container"
 import { Empty } from "./components/Empty"
 import { List } from "./components/List"
-import { Search } from "./components/Search"
+import { RecentCard } from "./components/RecentCard"
+import { RecentList } from "./components/RecentList"
 
 export const Posts = {
   Card,
   Container,
   Empty,
   List,
-  Search
+  RecentCard,
+  RecentList
 }
