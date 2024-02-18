655710d090578cde888da52f6f95149f1074d09d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-18 16:29:07 -0300

    feat: Update List function to filter and sort completed posts

--- src/components/Posts/components/List.tsx
+++ src/components/Posts/components/List.tsx
@@ -1,13 +1,19 @@
 import { Card } from "./Card"
 
-export function List({ posts }: PostsListProps): JSX.Element {
+export function List({ posts }: IPostsListProps): JSX.Element {
+  const lastPosts = posts
+    .slice()
+    .filter((post: INotionPost) => post.status === "Concluído")
+    .sort(
+      (a, b) =>
+        new Date(b.published_at).getTime() - new Date(a.published_at).getTime()
+    )
+
   return (
-    <ul className="w-full min-h-[221px] my-2 py-2 flex flex-col">
-      {posts
-        .filter((post: Post) => post.published)
-        .map((post: Post) => (
-          <Card key={post.slug} {...post} />
-        ))}
+    <ul className="w-full min-h-[221px] flex flex-col space-y-4">
+      {lastPosts.map((post: INotionPost) => (
+        <Card key={post.slug} {...post} />
+      ))}
     </ul>
   )
 }
