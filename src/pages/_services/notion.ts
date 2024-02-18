8562475d4d9ad0e21e8afd0ae23f4d9bde0b0542
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-18 14:17:51 -0300

    refactor: Update types and properties in Notion service

--- src/pages/_services/notion.ts
+++ src/pages/_services/notion.ts
@@ -9,21 +9,24 @@ async function fetchNotionPosts() {
   const response = await notion.databases.query({
     database_id: databaseId
   })
-  return response.results as NotionResult[]
+  return response.results as INotionResult[]
 }
 
-function mapToFormattedPost(post: NotionResult): NotionGSP {
+function mapToFormattedPost(post: INotionResult): INotionPost {
   return {
     title: post.properties.title.title[0].plain_text,
-    slug: post.properties.slug.rich_text[0].plain_text,
+    slug: post.properties.slug.formula.string,
+    status: post.properties.status.status.name,
     description: post.properties.description.rich_text[0].plain_text,
     tags: post.properties.tags.multi_select.map((tag) => tag.name),
+    reading_time: post.properties.reading_time.number,
+    created_by: post.properties.created_by.created_by.id,
     published_at: post.properties.published_at.date.start
   }
 }
 
-function extractSlugs(posts: NotionResult[]): string[] {
-  return posts.map((post) => post.properties.slug.rich_text[0].plain_text)
+function extractSlugs(posts: INotionResult[]): string[] {
+  return posts.map((post) => post.properties.slug.formula.string)
 }
 
 export async function getSlugs(): Promise<string[]> {
@@ -31,7 +34,7 @@ export async function getSlugs(): Promise<string[]> {
   return extractSlugs(posts)
 }
 
-export async function getPosts(): Promise<NotionGSP[]> {
+export async function getPosts(): Promise<INotionPost[]> {
   const posts = await fetchNotionPosts()
   return posts.map(mapToFormattedPost)
 }
@@ -53,7 +56,7 @@ export async function getPost(slug: string): Promise<INotionFormattedPost> {
   const mdBlocks = await n2m.pageToMarkdown(pageId)
   const mdStrings = n2m.toMarkdownString(mdBlocks)
 
-  const post = response.results[0] as NotionResult
+  const post = response.results[0] as INotionResult
   const formattedPost = mapToFormattedPost(post)
 
   return {
