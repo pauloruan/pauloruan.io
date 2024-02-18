652e66d5b935ea7f7ab40d6437475c85c21b6346
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-18 16:33:26 -0300

    feat: Update RecentPosts component

--- src/components/RecentPosts/index.tsx
+++ src/components/RecentPosts/index.tsx
@@ -1,21 +1,29 @@
-import { Posts } from "@/components/Posts"
-import { GlobalContext } from "@contexts/GlobalContext"
+import { Button } from "@/components/ui/button"
+import { GlobalContext } from "@/contexts/GlobalContext"
+import Link from "next/link"
 import { useContext } from "react"
+import { FiArrowUpRight } from "react-icons/fi"
+import { Posts } from "../Posts"
 
 export function RecentPosts(): JSX.Element {
   const { posts } = useContext(GlobalContext)
-  const lastPosts = posts
-  const lastPostsList = lastPosts.slice(0, 3)
 
   return (
-    <div className="w-full h-full flex flex-col justify-center items-start my-2 py-2 space-y-4">
-      <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-black dark:text-white font-sans my-1 py-1">
-        Últimos textos que escrevi
+    <div className="w-full h-full flex flex-col justify-center items-start my-1 py-2 px-2 space-y-2">
+      <h3 className="text-lg md:text-xl max-w-max max-h-max font-semibold text-black dark:text-white font-sans my-1">
+        Textos recentes
       </h3>
-      <div className="w-full h-full grid grid-cols-1 space-y-2">
-        {lastPostsList.map((post) => (
-          <Posts.Card key={post.slug} {...post} />
-        ))}
+      <div className="w-full h-full flex flex-col justify-center items-start gap-4">
+        <Posts.RecentList posts={posts} />
+        <Button asChild variant="outline">
+          <Link
+            href="/blog"
+            className="flex justify-center items-center text-center gap-1 font-medium text-primary dark:text-primary"
+          >
+            Ver todos os textos
+            <FiArrowUpRight className="h-4 w-4 text-primary dark:text-primary" />
+          </Link>
+        </Button>
       </div>
     </div>
   )
