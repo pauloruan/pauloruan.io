18dda75dcca978f65c66a1f625fd2b5c5cfc275d
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-02-18 16:52:14 -0300

    feat: Update footer of the page

--- src/components/shared/Footer/index.tsx
+++ src/components/shared/Footer/index.tsx
@@ -4,13 +4,15 @@ export function Footer(): JSX.Element {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="box-border w-full max-w-[666px] mx-auto h-32 flex flex-col flex-nowrap items-start justify-between text-center font-sans text-xs px-4 py-4 my-2 border-0 border-t border-cod-gray-300 dark:border-cod-gray-600">
-      <NowPlaying />
-      <div className="max-w-max font-sans font-normal text-left text-cod-gray-800 dark:text-cod-gray-200 leading-relaxed tracking-wide">
-        <p>
-          &copy; <time>2023</time> - <time>{currentYear}</time> • Paulo Ruan
-        </p>
-        <p>Desenvolvido com Next.js & Tailwind CSS.</p>
+    <footer className="box-border w-full max-w-[666px] mx-auto h-32 flex flex-col flex-nowrap items-start justify-between text-center font-sans text-xs px-4 py-4 my-2">
+      <div className="w-full h-full flex flex-col justify-center items-start my-1 py-1 px-2 space-y-1">
+        <NowPlaying />
+        <div className="max-w-max font-sans font-normal text-left text-primary dark:text-primary leading-relaxed tracking-wide">
+          <p>
+            &copy; <time>2023</time> - <time>{currentYear}</time> • Paulo Ruan
+          </p>
+          <p>Desenvolvido com Next.js & Tailwind CSS.</p>
+        </div>
       </div>
     </footer>
   )
