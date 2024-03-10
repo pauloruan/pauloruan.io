a7c58415d78b755abc725b83231514b846b8447c
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-09 23:57:29 -0300

    refactor: Add custom text color

--- src/components/shared/SocialMediaList/index.tsx
+++ src/components/shared/SocialMediaList/index.tsx
@@ -4,10 +4,11 @@ import { LinkCard } from "../LinkCard"
 export function SocialMediaList(): JSX.Element {
   const orderedSocialMedias = socialMedias
     .slice()
+    .filter(({ name }: ISocialMedia) => name !== "Email")
     .sort((a, b) => a.order - b.order)
 
   return (
-    <div className="flex flex-row justify-between items-center w-64">
+    <div className="w-48 flex flex-row justify-start items-center py-2 gap-2">
       {orderedSocialMedias.map((media: ISocialMedia) => (
         <LinkCard key={media.slug} data={media} />
       ))}
