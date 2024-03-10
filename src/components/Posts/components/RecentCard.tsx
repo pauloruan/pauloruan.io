203e213901d9dd872763e6d4a3bd15924a8e104c
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-10 00:00:17 -0300

    refactor: Update CardContainer style

--- src/components/Posts/components/RecentCard.tsx
+++ src/components/Posts/components/RecentCard.tsx
@@ -10,11 +10,11 @@ import { FiArrowUpRight } from "react-icons/fi"
 export function RecentCard(post: INotionPost): JSX.Element {
   return (
     <Link href={`/blog/${post.slug}`}>
-      <CardContainer className="h-full w-full rounded-md transition-all duration-300 ease-in-out hover:shadow-lg group">
+      <CardContainer className="h-full w-full rounded-md transition-all duration-300 ease-in-out hover:shadow-lg group hover:bg-secondary">
         <CardHeader className="p-2">
-          <CardTitle className="h-6 max-w-max flex justify-start items-center gap-1 text-center truncate">
+          <CardTitle className="h-6 w-full flex justify-start items-center gap-1 text-center truncate">
+            <FiArrowUpRight className="h-4 w-4 text-sm font-medium" />
             {post.title}
-            <FiArrowUpRight className="h-4 w-4 text-sm font-medium transition-all duration-300 ease-in-out" />
           </CardTitle>
           <CardDescription className="truncate">
             {post.description}
