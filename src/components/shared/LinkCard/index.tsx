0898f87bfbf7a4ad16501d120845f967673b3d89
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-09 23:55:13 -0300

    refactor: Add Accessible Title and Label to LinkCard

--- src/components/shared/LinkCard/index.tsx
+++ src/components/shared/LinkCard/index.tsx
@@ -8,7 +8,9 @@ export function LinkCard({ data }: ILinkCardProps): JSX.Element {
       href={url}
       passHref
       target="_blank"
-      className="p-2 max-h-max max-w-max flex flex-row items-center justify-center text-center gap-1 text-primary dark:text-primary font-sans text-sm font-normal transition-all duration-300 ease-in-out group rounded-md bg-card dark:bg-card hover:underline cursor-pointer shadow-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent dark:hover:text-accent-foreground"
+      title={`Visitar ${name}`}
+      aria-label={`Visitar ${name}`}
+      className="p-2 max-h-max max-w-max flex flex-row items-center justify-center text-center gap-1 text-primary dark:text-primary font-sans text-sm font-normal transition-all duration-300 ease-in-out group rounded-md bg-card dark:bg-card hover:underline cursor-pointer shadow-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent dark:hover:text-accent-foreground border"
     >
       <Icon className="h-4 w-4 text-primary dark:text-primary text-sm font-medium transition-all duration-300 ease-in-out group-hover:text-accent-foreground dark:group-hover:text-accent-foreground" />
       {renderLabel ? label : name}
