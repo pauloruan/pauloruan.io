bd627d81e0f757048f4e6a3454c371a77c7ee995
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-10 23:57:59 -0300

    refactor: Update PostContainer to use ReactMarkdown

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -1,39 +1,30 @@
 import { components } from "@/components/PortableText"
 import { convertToBrazilianDate } from "@/utils/convertToBrazilianDate"
 import { readingTime } from "@/utils/readTime"
-import { urlFor } from "@/utils/url-for"
-import { PortableText } from "@portabletext/react"
-import Image from "next/image"
+import ReactMarkdown from "react-markdown"
 
-export function PostContainer({ post }: PostProps): JSX.Element {
-  const { title, authorName, authorImage, date, body } = post
+interface PostContainerProps {
+  post: INotionFormattedPost
+}
 
+export function PostContainer({ post }: PostContainerProps) {
   return (
-    <section className="box-border w-full h-full flex flex-col items-start justify-center gap-5">
+    <section className="box-border w-full h-full flex flex-col items-start justify-center gap-5 px-2">
       <div className="w-full flex flex-col py-4 gap-1 rounded-sm justify-start items-start text-start text-black dark:text-white">
-        <h1 className="max-w-max h-full font-sans text-4xl font-black text-start my-1 py-1 text-black dark:text-white">
-          {title}
-        </h1>
+        <h3 className="max-w-max h-full font-sans text-xl font-bold text-start my-1 py-1 text-black dark:text-white">
+          {post.title}
+        </h3>
         <div className="flex flex-row justify-center items-center gap-4">
-          <Image
-            src={urlFor(authorImage).url()}
-            alt={authorName}
-            width={48}
-            height={48}
-            className="rounded-full shadow-md w-10 h-10"
-          />
           <div className="max-w-max font-sans leading-relaxed text-xs font-normal text-start text-black dark:text-white">
-            <p>{authorName}</p>
             <p>
-              <time>{convertToBrazilianDate(date)}</time> -&nbsp;
-              {readingTime(body)}
+              <time>{convertToBrazilianDate(post.published_at)}</time> -&nbsp;
+              {readingTime(post.reading_time)}
             </p>
           </div>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-start text-start text-black dark:text-white">
-        <PortableText value={body} components={components} />
-        <hr className="w-full h-px bg-cod-gray-200 dark:bg-cod-gray-800 my-6" />
+      <div className="w-full h-full flex flex-col justify-center items-start text-start text-black dark:text-white space-y-2">
+        <ReactMarkdown components={components}>{post.content}</ReactMarkdown>
       </div>
     </section>
   )
