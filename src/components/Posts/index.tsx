8e46482cb73c5df1befb250bb51368e272e1b4fa
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:34:17 -0300

    refactor: Remove component RecentCard

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -2,7 +2,6 @@ import { Card } from "./components/Card"
 import { Container } from "./components/Container"
 import { Empty } from "./components/Empty"
 import { List } from "./components/List"
-import { RecentCard } from "./components/RecentCard"
 import { RecentList } from "./components/RecentList"
 
 export const Posts = {
@@ -10,6 +9,5 @@ export const Posts = {
   Container,
   Empty,
   List,
-  RecentCard,
   RecentList
 }
