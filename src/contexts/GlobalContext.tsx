f2848e4db29d9d7ab10b31042f7f0790a80d47a5
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-16 23:42:39 -0300

    feat: Add global variables

--- src/contexts/GlobalContext.tsx
+++ src/contexts/GlobalContext.tsx
@@ -1,29 +1,41 @@
 import * as React from "react"
 
 interface GlobalContextProps {
-  posts: INotionPost[]
-  setPosts: React.Dispatch<React.SetStateAction<INotionPost[]>>
+  posts: IPost[]
+  setPosts: React.Dispatch<React.SetStateAction<IPost[]>>
+  about: IAbout
+  setAbout: React.Dispatch<React.SetStateAction<IAbout>>
+  experiencies: IExperience[]
+  setExperiencies: React.Dispatch<React.SetStateAction<IExperience[]>>
 }
 
 const DEFAULT_VALUE = {
-  posts: [],
-  setPosts: () => {}
+  posts: [] as IPost[],
+  setPosts: () => {},
+  about: {} as IAbout,
+  setAbout: () => {},
+  experiencies: [] as IExperience[],
+  setExperiencies: () => {}
 }
 
 export const GlobalContext =
   React.createContext<GlobalContextProps>(DEFAULT_VALUE)
 
-export function GlobalProvider({
-  children
-}: IGlobalProviderProps): JSX.Element {
-  const [posts, setPosts] = React.useState<INotionPost[]>([])
+export function GlobalProvider({ children }: IGlobalProviderProps) {
+  const [posts, setPosts] = React.useState<IPost[]>([])
+  const [about, setAbout] = React.useState<IAbout>({} as IAbout)
+  const [experiencies, setExperiencies] = React.useState<IExperience[]>([])
 
   const memoizedValue = React.useMemo(
     () => ({
       posts,
-      setPosts
+      setPosts,
+      about,
+      setAbout,
+      experiencies,
+      setExperiencies
     }),
-    [posts]
+    [posts, about, experiencies]
   )
 
   return (
