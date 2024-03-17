ec177f13f9e4b41cafcbc22ee537e16280e3493a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:35:24 -0300

    refactor: Update the AboutContent component

--- src/components/AboutContent/index.tsx
+++ src/components/AboutContent/index.tsx
@@ -1,28 +1,20 @@
-import { components } from "@/components/PortableText"
-import { FrequentTechnologies } from "@/components/shared/FrequentTechnologies"
-import { Medias } from "@/components/shared/SocialMediaList"
+import { sanityComponents } from "@/components/PortableText"
+import { Typography } from "@/components/Typography"
+import { Experience } from "@/components/content/Experience"
+import { Contact } from "@/components/shared/Contact"
+import { Work } from "@/components/shared/Work"
 import { PortableText } from "@portabletext/react"
 
-export function AboutContent({ content, contact }: IAbout): JSX.Element {
+export function AboutContent({ content }: IAbout) {
   return (
-    <div className="w-full h-full flex flex-col items-start justify-start my-4 gap-5">
-      <PortableText
-        aria-label="Sobre mim"
-        value={content}
-        components={components}
-      />
-      <FrequentTechnologies />
-      <div className="w-full h-full flex flex-col items-start justify-start">
-        <h3 className="text-2xl font-bold text-black dark:text-white font-sans my-2 py-1">
-          Contato
-        </h3>
-        <PortableText
-          aria-label="Contato"
-          value={contact}
-          components={components}
-        />
-        <Medias />
+    <div className="w-full h-full flex flex-col items-start justify-start space-y-4">
+      <div className="w-full h-full px-2">
+        <Typography.H1 className="mb-4">Sobre Mim</Typography.H1>
+        <PortableText value={content} components={sanityComponents} />
       </div>
+      <Work />
+      <Experience />
+      <Contact />
     </div>
   )
 }
