de7b866906107a37ff35f004559851096f287831
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-16 23:54:51 -0300

    refactor: Replace h1 with Typography.H1

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,10 +1,12 @@
+import { Typography } from "@/components/Typography"
+
 export default function Error({ statusCode }: { statusCode: number }) {
   return (
-    <h1 className="h-screen w-screen flex items-center justify-center font-sans font-black">
+    <Typography.H1 className="h-screen w-screen flex items-center justify-center font-sans font-black">
       {statusCode
         ? `An error ${statusCode} occurred on server`
         : "An error occurred on client"}
-    </h1>
+    </Typography.H1>
   )
 }
 
