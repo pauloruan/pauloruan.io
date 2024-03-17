46bf3ddc99617cabb4288f148062c2aecca8d666
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:36:48 -0300

    refactor: Add title

--- src/components/BlogContent/index.tsx
+++ src/components/BlogContent/index.tsx
@@ -1,16 +1,20 @@
 import { Posts } from "@/components/Posts"
 import { GlobalContext } from "@/contexts/GlobalContext"
 import { useContext } from "react"
+import { Typography } from "../Typography"
 
 export function BlogContent(): JSX.Element {
   const { posts } = useContext(GlobalContext)
 
-  const filteredPosts = posts.filter((post) => post.status === "Concluído")
+  const filteredPosts = posts.slice().filter((post) => post.published)
 
   return (
-    <Posts.Container>
-      {filteredPosts.length === 0 && <Posts.Empty />}
-      {filteredPosts.length > 0 && <Posts.List posts={filteredPosts} />}
-    </Posts.Container>
+    <div className="w-full h-full flex flex-col items-start justify-center gap-8">
+      <Typography.H1>Blog</Typography.H1>
+      <Posts.Container>
+        {filteredPosts.length === 0 && <Posts.Empty />}
+        {filteredPosts.length > 0 && <Posts.List posts={filteredPosts} />}
+      </Posts.Container>
+    </div>
   )
 }
