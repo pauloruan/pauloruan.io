fce7e86c8598f7b1c856b45f26fe993e564f770a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:03:18 -0300

    refactor: Update Item Styles

--- src/components/shared/Header/Menu/components/Item.tsx
+++ src/components/shared/Header/Menu/components/Item.tsx
@@ -11,7 +11,7 @@ export function Item({ name, path }: INavLink): JSX.Element {
       target="_self"
       aria-label={name}
       data-current-path={currentPath}
-      className="h-8 px-1 max-w-max no-underline text-sm md:text-base text-primary dark:text-primary font-medium cursor-pointer flex items-center justify-center text-center transition duration-300 ease-in-out data-[current-path='true']:font-semibold data-[current-path='true']:underline data-[current-path='true']:underline-offset-2 data-[current-path='true']:decoration-accent"
+      className="h-8 px-1 max-w-max no-underline text-sm md:text-sm text-primary/80 dark:text-primary/80 font-medium cursor-pointer flex items-center justify-center text-center transition duration-300 ease-in-out data-[current-path='true']:font-bold data-[current-path='true']:text-primary dark:data-[current-path='true']:text-primary"
     >
       {name}
     </Link>
