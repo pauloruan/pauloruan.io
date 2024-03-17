ed569a53fbf6179143bd189108856317a5b1c066
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:02:26 -0300

    refactor: Remove title property

--- src/components/shared/SectionContainer/index.tsx
+++ src/components/shared/SectionContainer/index.tsx
@@ -1,15 +1,10 @@
 import { ScrollToTopButton } from "@/components/shared/ScrollToTopButton"
 
-export function SectionContainer(props: ISectionContainerProps): JSX.Element {
+export function SectionContainer({ children }: ISectionContainerProps) {
   return (
     <section className="box-border w-full h-full min-h-[70vh] max-w-[666px] mx-auto flex flex-col items-start justify-center my-2 px-4 py-2">
-      {props.title && (
-        <h1 className="max-w-max h-full font-sans text-3xl font-black text-left py-1 text-black dark:text-white leading-relaxed tracking-tight">
-          {props.title}
-        </h1>
-      )}
       <div className="min-h-[70vh] h-full w-full flex flex-col items-start justify-start">
-        {props.children}
+        {children}
       </div>
       <ScrollToTopButton />
     </section>
