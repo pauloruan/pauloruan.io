b1332c9339fe2afedd2f292d095443e0e23458d1
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:36:02 -0300

    refactor: Use the new components and correct property names

--- src/components/PostContainer/index.tsx
+++ src/components/PostContainer/index.tsx
@@ -1,30 +1,25 @@
-import { components } from "@/components/PortableText"
+import { sanityComponents } from "@/components/PortableText"
+import { Typography } from "@/components/Typography"
 import { convertToBrazilianDate } from "@/utils/convertToBrazilianDate"
-import { readingTime } from "@/utils/readTime"
-import ReactMarkdown from "react-markdown"
+import { PortableText } from "@portabletext/react"
 
-interface PostContainerProps {
-  post: INotionFormattedPost
+interface IPostContainerProps {
+  post: IPost
 }
 
-export function PostContainer({ post }: PostContainerProps) {
+export function PostContainer({ post }: IPostContainerProps) {
   return (
     <section className="box-border w-full h-full flex flex-col items-start justify-center gap-5 px-2">
       <div className="w-full flex flex-col py-4 gap-1 rounded-sm justify-start items-start text-start text-black dark:text-white">
-        <h3 className="max-w-max h-full font-sans text-xl font-bold text-start my-1 py-1 text-black dark:text-white">
-          {post.title}
-        </h3>
+        <Typography.H1>{post.title}</Typography.H1>
         <div className="flex flex-row justify-center items-center gap-4">
-          <div className="max-w-max font-sans leading-relaxed text-xs font-normal text-start text-black dark:text-white">
-            <p>
-              <time>{convertToBrazilianDate(post.published_at)}</time> -&nbsp;
-              {readingTime(post.reading_time)}
-            </p>
-          </div>
+          <time className="max-w-max font-sans leading-relaxed text-xs font-normal text-start text-black dark:text-white">
+            {convertToBrazilianDate(post.publishedAt)}
+          </time>
         </div>
       </div>
       <div className="w-full h-full flex flex-col justify-center items-start text-start text-black dark:text-white space-y-2">
-        <ReactMarkdown components={components}>{post.content}</ReactMarkdown>
+        <PortableText value={post.body} components={sanityComponents} />
       </div>
     </section>
   )
