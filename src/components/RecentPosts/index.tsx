4a3c6ac92f8497db620046b52dea57cdf9945756
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-17 00:08:33 -0300

    refactor: Update Typography Components

--- src/components/RecentPosts/index.tsx
+++ src/components/RecentPosts/index.tsx
@@ -1,17 +1,16 @@
+import { Posts } from "@/components/Posts"
+import { Typography } from "@/components/Typography"
 import { GlobalContext } from "@/contexts/GlobalContext"
 import Link from "next/link"
 import { useContext } from "react"
 import { FiArrowUpRight } from "react-icons/fi"
-import { Posts } from "../Posts"
 
 export function RecentPosts(): JSX.Element {
   const { posts } = useContext(GlobalContext)
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-start my-1 py-2 px-2 space-y-2">
-      <h3 className="text-lg md:text-xl max-w-max max-h-max font-semibold text-black dark:text-white font-sans my-1">
-        Textos recentes
-      </h3>
+      <Typography.H3>Textos recentes</Typography.H3>
       <div className="w-full h-full flex flex-col justify-center items-start gap-4">
         <Posts.RecentList posts={posts} />
 
