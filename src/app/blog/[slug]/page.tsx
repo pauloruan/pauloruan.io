1168914a9ddd4ff22de271d7adb0ebe35eeae2d8
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-25 02:14:42 -0300

    refactor: Update blog post maintenance

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,42 +1,45 @@
-import { PostContainer } from "@/components/PostContainer"
+import { MaintenanceContent } from "@/components/MaintenanceContent"
+// import { PostContainer } from "@/components/PostContainer"
 import { SectionContainer } from "@/components/shared/SectionContainer"
-import { getSanityPostBySlug, getSanityPostSlugs } from "@/lib/sanity.queries"
-import { Metadata } from "next"
+// import { getSanityPostBySlug, getSanityPostSlugs } from "@/lib/sanity.queries"
+// import { Metadata } from "next"
 
-interface PostProps {
-  params: { slug: string }
-}
+// interface PostProps {
+//   params: { slug: string }
+// }
 
-interface Slug {
-  slug: string
-}
+// interface Slug {
+//   slug: string
+// }
 
-export async function generateMetadata({
-  params
-}: PostProps): Promise<Metadata> {
-  const post = await getSanityPostBySlug(params.slug)
+// export async function generateMetadata({
+//   params
+// }: PostProps): Promise<Metadata> {
+//   const post = await getSanityPostBySlug(params.slug)
 
-  return {
-    title: post.title,
-    description: post.description,
-    openGraph: {
-      url: `https://pauloruan.dev/blog/${params.slug}`,
-      type: "article"
-    }
-  }
-}
+//   return {
+//     title: post.title,
+//     description: post.description,
+//     openGraph: {
+//       url: `https://pauloruan.dev/blog/${params.slug}`,
+//       type: "article"
+//     }
+//   }
+// }
 
-export async function generateStaticParams() {
-  const slugs = await getSanityPostSlugs()
-  return slugs.map(({ slug }: Slug) => ({ slug }))
-}
+// export async function generateStaticParams() {
+//   const slugs = await getSanityPostSlugs()
+//   return slugs.map(({ slug }: Slug) => ({ slug }))
+// }
 
-export default async function Post({ params }: PostProps) {
-  const post = await getSanityPostBySlug(params.slug)
+// export default async function Post({ params }: PostProps) {
+export default async function Post() {
+  // const post = await getSanityPostBySlug(params.slug)
 
   return (
     <SectionContainer>
-      <PostContainer post={post} slug={params.slug} />
+      {/* <PostContainer post={post} slug={params.slug} /> */}
+      <MaintenanceContent />
     </SectionContainer>
   )
 }
