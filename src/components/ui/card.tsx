ab73bcdbea3ea10a04ab73b575359c32e56d831a
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-25 01:49:19 -0300

    refactor: Adjust lint fixes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
@@ -73,4 +74,4 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
