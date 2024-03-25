2d5fb5e1df145e0edbd51f72c7ddb9fa2e2f05b6
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-25 14:04:30 -0300

    refactor: Update loading style to take up the entire screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { Typography } from "@/components/Typography"
 
 export default function Loading() {
   return (
-    <div className="w-full h-full flex items-center justify-center">
+    <div className="w-screen h-screen flex items-center justify-center">
       <Typography.H1 className="animate-pulse">Carregando...</Typography.H1>
     </div>
   )
